d5a629b0294b54e87b591273393339e300b390ce
Stypox <stypox@pm.me>
2021-08-06 13:35:56 +0200

    Add all arguments

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,12 @@
 #include "game/game.hpp"
 #include "ai/scoring_function.hpp"
 
-void parseArguments(int argc, char const* argv[], bool& noUi, ai::scoring_table_t& scoringTable) {
+void parseArguments(int argc,
+					char const* argv[],
+					bool& noUi,
+					ai::scoring_table_t& scoringTable,
+					std::pair<float, int>& connectedComponentsParams,
+					float& biggestRectangleMaxScore) {
 	bool help = false;
 	bool usage = false;
 
@@ -16,9 +21,13 @@ void parseArguments(int argc, char const* argv[], bool& noUi, ai::scoring_table_
 			stypox::SwitchOption{"help", help, stypox::args("-h", "--help"), "show help screen"},
 			stypox::SwitchOption{"usage", usage, stypox::args("-u", "--usage"), "show usage screen"},
 			stypox::HelpSection{"\nGame options:"},
-			stypox::SwitchOption{"no ui", noUi, stypox::args("-n", "--no-ui"), "do not start an sfml application but just run the game and print out the results"},
-			stypox::HelpSection{"\nAI options (only the scoring functions related to the provided arguments will be used,\n    though if no option is selected, FittingPiecesScoringFunction with fastScoringTable will be used):"},
-			stypox::ManualOption{"scoring table", scoringTable, stypox::args("-fpst=", "--fitting-pieces-scoring-table="), "a scoring table, enables FittingPiecesScoringFunction (possible values: full, fast, custom)",
+			stypox::SwitchOption{"no ui", noUi, stypox::args("-n", "--no-ui"),
+				"do not start an sfml application but just run the game and print out the results"},
+			stypox::HelpSection{"\nAI options (only the scoring functions related to the provided arguments will be used,"
+				"though if no option is selected, FittingPiecesScoringFunction with fastScoringTable will be used):"},
+			stypox::ManualOption{"fitting pieces scoring table", scoringTable,
+				stypox::args("-fpst=", "--fitting-pieces-scoring-table="),
+				"a scoring table for FittingPiecesScoringFunction (possible values: full, fast, custom)",
 				[](const std::string_view& str) {
 					if (str == "full") {
 						return ai::fullScoringTable;
@@ -29,7 +38,37 @@ void parseArguments(int argc, char const* argv[], bool& noUi, ai::scoring_table_
 					} else {
 						throw std::runtime_error{"Invalid scoring table: " + std::string{str}};
 					}
-				}}
+				}},
+			stypox::ManualOption{"connected components parameters", connectedComponentsParams,
+				stypox::args("-ccp=", "--connected-components-parameters="),
+				"the max score (>= 0) and the minimum size of non penalized groups (> 0 and <= number of"
+				" squares) for ConnectedComponentsScoringFunction (format: `score:size` [`float:int`])",
+				[](const std::string_view& str) {
+					std::pair<float, int> result;
+					try {
+						size_t semicolonIndex = str.find_first_of(":");
+						if (semicolonIndex == std::string::npos)
+							throw std::runtime_error{""};
+
+						std::string maxScore{str.substr(0, semicolonIndex)},
+							penalizeSmallerThan{str.substr(semicolonIndex + 1, str.size() - semicolonIndex - 1)};
+						result = {std::stof(maxScore), std::stoi(penalizeSmallerThan)};
+					} catch (...) {
+						throw std::runtime_error{"Badly formatted connected components parameters: " + std::string{str}};
+					}
+
+					if (result.first < 0) {
+						throw std::runtime_error{"Max score must be >= 0 in connected components parameters: " + std::to_string(result.first)};
+					} else if (result.second <= 0 || result.second > app::BOARD_SIZE * app::BOARD_SIZE) {
+						throw std::runtime_error{"Penalize when smaller must be > 0 and <= number of squares in connected components parameters: " + std::to_string(result.second)};
+					}
+					return result;
+				}},
+			stypox::Option{"biggest rectangle max score", biggestRectangleMaxScore,
+				stypox::args("-brms=", "--biggest-rectangle-max-score="),
+				"the max score (>= 0) for BiggestRectangleScoringFunction",
+				false, // option not required
+				[](float value){ return value >= 0; }}
 		),
 		"1010! AI by Stypox"
 	};
@@ -52,11 +91,12 @@ void parseArguments(int argc, char const* argv[], bool& noUi, ai::scoring_table_
 int main(int argc, char const* argv[]) {
 	bool noUi = false;
 	ai::scoring_table_t scoringTable;
-	parseArguments(argc, argv, noUi, scoringTable);
+	std::pair<float, int> connectedComponentsParams{0.0f, 1};
+	float biggestRectangleMaxScore = 0.0f;
+	parseArguments(argc, argv, noUi,
+		scoringTable, connectedComponentsParams, biggestRectangleMaxScore);
 
 	// run application
-//            + ai::ConnectedComponentsScoringFunction{0.03f, 3}
-//            + ai::BiggestRectangleScoringFunction{0.01f}
     if (noUi) {
 		app::NoUiApplication{game::Game{{ai::FittingPiecesScoringFunction{scoringTable}}}}.run();
     } else {
