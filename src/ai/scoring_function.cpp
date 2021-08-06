0f3fea127184cf148e68cc615473d214de918860
Stypox <stypox@pm.me>
2021-08-06 11:16:37 +0200

    Make scoring tables extern to fix strange undefined behaviors

--- src/ai/scoring_function.cpp
+++ src/ai/scoring_function.cpp
@@ -1,12 +1,69 @@
 #include "scoring_function.hpp"
 
 #include <stack>
+#include <iostream>
 
 namespace ai {
 
+// the numerator represents how valuable it is to have an available space for that piece
+// the denominator is the maximum number of occourrences of the piece
+const std::vector<std::pair<game::Piece::id_t, float>> fullScoringTable {
+	std::pair<game::Piece::id_t, float> // needs to be specified for the first item
+	{game::pieceLong5Vertical.id,   0.25f /  60},
+	{game::pieceLong4Vertical.id,   0.15f /  70},
+	{game::pieceLong3Vertical.id,   0.07f /  80},
+	{game::pieceLong2Vertical.id,   0.03f /  90},
+	{game::pieceLong5Horizontal.id, 0.25f /  60},
+	{game::pieceLong4Horizontal.id, 0.15f /  70},
+	{game::pieceLong3Horizontal.id, 0.07f /  80},
+	{game::pieceLong2Horizontal.id, 0.03f /  90},
+	{game::pieceSquare3x3.id,       1.00f /  64},
+	{game::pieceSquare2x2.id,       0.50f /  81},
+	{game::pieceAngle3x3TL.id,      0.20f /  64},
+	{game::pieceAngle3x3TR.id,      0.20f /  64},
+	{game::pieceAngle3x3BR.id,      0.20f /  64},
+	{game::pieceAngle3x3BL.id,      0.20f /  64},
+	{game::pieceAngle2x2TL.id,      0.10f /  81},
+	{game::pieceAngle2x2TR.id,      0.10f /  81},
+	{game::pieceAngle2x2BR.id,      0.10f /  81},
+	{game::pieceAngle2x2BL.id,      0.10f /  81},
+	{game::pieceSingle.id,          0.05f / 100},
+};
+
+const std::vector<std::pair<game::Piece::id_t, float>> fastScoringTable {
+	std::pair<game::Piece::id_t, float> // needs to be specified for the first item
+	{game::pieceLong5Vertical.id,   0.25f /  60},
+	{game::pieceLong5Horizontal.id, 0.25f /  60},
+	{game::pieceSquare3x3.id,       1.00f /  64},
+	{game::pieceSquare2x2.id,       0.50f /  81},
+	{game::pieceAngle3x3TL.id,      0.10f /  64},
+	{game::pieceAngle3x3TR.id,      0.10f /  64},
+	{game::pieceAngle3x3BR.id,      0.10f /  64},
+	{game::pieceAngle3x3BL.id,      0.10f /  64},
+	{game::pieceSingle.id,          0.05f / 100},
+};
+
+const std::vector<std::pair<game::Piece::id_t, float>> customScoringTable {
+	std::pair<game::Piece::id_t, float> // needs to be specified for the first item
+	{game::pieceLong5Vertical.id,   0.25f /  60},
+	{game::pieceLong4Vertical.id,   0.04f /  70},
+	{game::pieceLong3Vertical.id,   0.02f /  80},
+	{game::pieceLong5Horizontal.id, 0.30f /  60},
+	{game::pieceLong4Horizontal.id, 0.04f /  70},
+	{game::pieceLong3Horizontal.id, 0.02f /  80},
+	{game::pieceSquare3x3.id,       1.00f /  64},
+	{game::pieceSquare2x2.id,       0.20f /  81},
+	{game::pieceAngle3x3TL.id,      0.05f /  64},
+	{game::pieceAngle3x3TR.id,      0.05f /  64},
+	{game::pieceAngle3x3BR.id,      0.05f /  64},
+	{game::pieceAngle3x3BL.id,      0.07f /  64},
+	{game::pieceSingle.id,          0.02f / 100},
+};
+
+
 FittingPiecesScoringFunction::FittingPiecesScoringFunction(
-		const std::vector<std::pair<game::Piece::id_t, float>>& scoringTable)
-		: scoringTable{scoringTable} {}
+		const std::vector<std::pair<game::Piece::id_t, float>>& theScoringTable)
+		: scoringTable{theScoringTable} {}
 
 float FittingPiecesScoringFunction::operator()(const game::Board& board) const {
 	float score = 0.01f; // always greater than 0, so that it compares better than the score used as the base case
