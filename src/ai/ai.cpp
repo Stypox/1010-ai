984e77305d95bb37ddbd3eb0af3d695742ce4610
Stypox <stypox@pm.me>
2021-08-19 15:57:42 +0200

    Optimize brute force ai function calling

--- src/ai/ai.cpp
+++ src/ai/ai.cpp
@@ -1,38 +1,6 @@
 #include "ai.hpp"
 
-#include "raw/raw_board.hpp"
-#include "raw/raw_piece.hpp"
-
 namespace ai {
-
-std::pair<float, std::vector<Move>> Ai::bruteForceWithScore(const raw_board_t& board, const std::vector<piece_id_t>& availablePieces) {
-	if (availablePieces.empty()) {
-		return {scoringFunction(board), {}};
-	}
-
-	const piece_id_t currentPieceId = availablePieces[0];
-	const std::vector<piece_id_t> nextAvailablePieces{availablePieces.begin() + 1, availablePieces.end()};
-
-	float bestScoreSoFar = 0.0f;
-	std::vector<Move> bestMovesSoFar;
-	for (int i = 0; i < app::BOARD_SIZE - raw::pieceHeight[currentPieceId] + 1; ++i) {
-		for (int j = 0; j < app::BOARD_SIZE - raw::pieceWidth[currentPieceId] + 1; ++j) {
-			if (raw::fitsPieceAtNoBoundaryChecks(board, i, j, currentPieceId)) {
-				raw_board_t newBoard = board;
-				raw::placePieceAt(newBoard, i, j, currentPieceId);
-
-				auto [score, moves] = bruteForceWithScore(newBoard, nextAvailablePieces);
-				if (score > bestScoreSoFar) {
-					bestScoreSoFar = score;
-					bestMovesSoFar = std::move(moves);
-					bestMovesSoFar.push_back({i, j, currentPieceId});
-				}
-			}
-		}
-	}
-
-	return {bestScoreSoFar, bestMovesSoFar};
-}
 
 Ai::Ai(const ai::scoring_function_t& scoringFunction)
 		: scoringFunction{scoringFunction} {}
@@ -43,10 +11,23 @@ std::vector<Move> Ai::bruteForce(const raw_board_t& board, std::vector<piece_id_
 	float bestScoreSoFar = 0.0f;
 	std::vector<Move> bestMovesSoFar;
 	do {
-		auto [score, moves] = bruteForceWithScore(board, availablePieces);
-		if (score > bestScoreSoFar) {
-			bestScoreSoFar = score;
-			bestMovesSoFar = moves;
+		std::pair<float, std::vector<Move>> result;
+		switch (availablePieces.size()) {
+		case 0:
+			break;
+		case 1:
+			result = bruteForceWithScore(board, availablePieces[0]);
+			break;
+		case 2:
+			result = bruteForceWithScore(board, availablePieces[0], availablePieces[1]);
+			break;
+		case 3:
+			result = bruteForceWithScore(board, availablePieces[0], availablePieces[1], availablePieces[2]);
+			break;
+		}
+		if (result.first > bestScoreSoFar) {
+			bestScoreSoFar = result.first;
+			bestMovesSoFar = result.second;
 		}
 	} while (std::next_permutation(availablePieces.begin(), availablePieces.end()));
 
@@ -63,7 +44,7 @@ std::vector<Move> Ai::bestCombinationOfSingleMoves(const raw_board_t& board, std
 		std::vector<Move> moves;
 		raw_board_t currentBoard = board;
 		for (auto availablePiece : availablePieces) {
-			auto [partialScore, partialMoves] = bruteForceWithScore(currentBoard, {availablePiece});
+			auto [partialScore, partialMoves] = bruteForceWithScore(currentBoard, availablePiece);
 			if (partialMoves.empty()) {
 				break; // has lost
 			}
--- src/ai/ai.hpp
+++ src/ai/ai.hpp
@@ -6,13 +6,40 @@
 #include "ai/move.hpp"
 #include "ai/scoring_function.hpp"
 #include "game/board.hpp"
+#include "raw/raw_board.hpp"
+#include "raw/raw_piece.hpp"
 
 namespace ai {
 
 class Ai {
 	const ai::scoring_function_t scoringFunction;
 
-	std::pair<float, std::vector<Move>> bruteForceWithScore(const raw_board_t& board, const std::vector<piece_id_t>& availablePieces);
+	inline std::pair<float, std::vector<Move>> bruteForceWithScore(const raw_board_t& board) {
+		return {scoringFunction(board), {}};
+	}
+
+	template<typename... Ts>
+	inline std::pair<float, std::vector<Move>> bruteForceWithScore(const raw_board_t& board, piece_id_t currentPieceId, Ts... nextAvailablePieces) {
+		float bestScoreSoFar = 0.0f;
+		std::vector<Move> bestMovesSoFar;
+		for (int i = 0; i < app::BOARD_SIZE - raw::pieceHeight[currentPieceId] + 1; ++i) {
+			for (int j = 0; j < app::BOARD_SIZE - raw::pieceWidth[currentPieceId] + 1; ++j) {
+				if (raw::fitsPieceAtNoBoundaryChecks(board, i, j, currentPieceId)) {
+					raw_board_t newBoard = board;
+					raw::placePieceAt(newBoard, i, j, currentPieceId);
+
+					auto [score, moves] = bruteForceWithScore(newBoard, nextAvailablePieces...);
+					if (score > bestScoreSoFar) {
+						bestScoreSoFar = score;
+						bestMovesSoFar = std::move(moves);
+						bestMovesSoFar.push_back({i, j, currentPieceId});
+					}
+				}
+			}
+		}
+
+		return {bestScoreSoFar, bestMovesSoFar};
+	}
 
 public:
 	Ai(const ai::scoring_function_t& scoringFunction);
